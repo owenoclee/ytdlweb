8c273a3b15909b2efb6bb4ef7157e3c6b740390f
Owen Oclee <owenoclee@users.noreply.github.com>
2020-05-16 20:20:56 +0100

    Put downloads into a subfolder and download subs

--- ytdl.go
+++ ytdl.go
@@ -6,7 +6,13 @@ import (
 )
 
 func download(url string) (io.Reader, error) {
-	ytdl := exec.Command("youtube-dl", "--newline", url)
+	ytdl := exec.Command(
+		"youtube-dl",
+		"--newline",
+		"--all-subs",
+		"-o 'downloads/%(title)s-%(id)s.%(ext)s'",
+		url,
+	)
 
 	stdout, err := ytdl.StdoutPipe()
 	if err != nil {
